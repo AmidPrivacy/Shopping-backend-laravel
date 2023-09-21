fd461a23657e9ad346205104e3669d7dd393855b
amid haqverdiyev <amid.haqverdiyev@limestonedigital.com>
2023-09-21 11:55:24 +0400

    add responsive style for home page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,8 +87,14 @@
                                 <div class="under-menu-box">
                                     @foreach($menu->categories as  $category)
                                     <div class="main-category-item">
-                                        <a href="/categories/{{ $category->uuid }}">{{ $category->name }}</a>
+                                        <a href="/categories/{{ $category->uuid }}">
+                                            {{ $category->name }}
+                                            @if(count($category->subs)>0)
+                                                <i class="fa-solid fa-chevron-right icon-down"></i>
+                                            @endif
+                                        </a>
                                         @if(count($category->subs)>0)
+                                        <i class="fa-solid fa-chevron-right icon-down"></i>
                                         <div class="sub-category-box">
                                             @foreach($category->subs as  $sub)
                                                 <a href="/sub-categories/{{ $sub->uuid }}">{{ $sub->name }}</a>
