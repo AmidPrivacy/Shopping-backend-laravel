7ec041c5d01780354ac293ade4d291313a4a19a8
amid haqverdiyev <amid.haqverdiyev@limestonedigital.com>
2023-10-01 19:34:26 +0400

    edit home page style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,15 +11,14 @@
         <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active" data-bs-interval="10000">
-                    <img src="assets/img/slide1.jpg" class="d-block w-100" alt="...">
+                    <img src="assets/img/banner-1.png" class="d-block w-100" alt="...">
                     <div class="carousel-caption d-none d-md-block">
                         <h3>First slide label</h3>
-                        <p>Some representative placeholder content for the first slide.</p>
-                        <a href="">Ətraflı</a>
+                        <p>Some representative placeholder content for the first slide.</p> 
                     </div>
                 </div>
                 <div class="carousel-item" data-bs-interval="2000">
-                    <img src="assets/img/slide2.jpg" class="d-block w-100" alt="...">
+                    <img src="assets/img/banner-2.png" class="d-block w-100" alt="...">
                     <div class="carousel-caption d-none d-md-block">
                         <h3>Second slide label</h3>
                         <p>Some representative placeholder content for the first slide.</p>
@@ -125,12 +124,7 @@
                                 <a href="/product/{{ $product->uuid }}">{{ $product->name }}</a>
                             </div>
                             <div class="intro">
-                                <span class="last-price">
-                                    <!-- <del class="old-price">
-                                        $
-                                        1200
-                                    </del> -->
-                                    
+                                <span class="last-price"> 
                                     {{ $product->price }} AZN
                                 </span>
                                 <div class="reyting">
@@ -147,62 +141,47 @@
                     </div> 
                     @endif
                 @endforeach
-            </div>
-
-        </div>
-
+            </div> 
+        </div> 
     </section> 
-
-    <!-- SPECIAL OFFER -->
-    <section class="special-offer">
-        <div class="bg-image">
-            <img src="assets/img/03.jpg" id="background-image" alt="">
-
-            <div class="bg-text">
-                <span class="limited-offer">MƏHDUD TƏKLİF</span>
-                <h2 class="bg-h2">
-                    Aşağıdakı məhsullara
-                    <br>
-                    <span class="bg-span">60% -dək</span>
-                    ENDİRİM
-                </h2> 
-            </div>
-
-        </div>
-
-    </section>
-
+  
     <!-- SECTION OUR PRODUCTS SLIDE -->
-    <section class="our-prod-slide">
-        <div class="container-fluid">
+    <section class="our-prod-slide section-product">
+        <div class="custom-container">
             <div class="row">
                 <div>
                     <h6 class="our-prod-new-collection">— Yeni Kalleksiya</h6>
-                    <h2 class="our-prod-our-product">Bizim Məhsullar</h2>
+                    <h2 class="our-prod-our-product">Populyar axtarışlar</h2>
                 </div>
-
-                <!-- <ul class="ul-nav-tabs">
-                    <li class="li-nav-item"><a href="#">Top Rated</a></li>
-                    <li class="li-nav-item"><a href="#">New Product</a></li>
-                    <li class="li-nav-item"><a href="#">Best Seller</a></li>
-                </ul> -->
-
-
-                <div id="left" class="wrapper">
+  
                     <!-- <i class="fa-solid fa-chevron-left"></i> -->
-                    <div class="carousel">
-                        <a href="#"><img src="assets/img/carouselimage1.jpeg" alt="" class="carousel-width"></a>
-                        <a href="#"><img src="assets/img/carouselimage2.jpeg" alt="" class="carousel-width"></a>
-                        <a href="#"><img src="assets/img/carouselimage3.jpeg" alt="" class="carousel-width"></a>
-                        <a href="#"><img src="assets/img/im2jpeg.jpeg" alt="" class="carousel-width"></a>
-                        <a href="#"><img src="assets/img/img1.peg.jpeg" alt="" class="carousel-width"></a>
-                        <a href="#"><img src="assets/img/img3.jpeg" alt="" class="carousel-width"></a>
-                    </div>
-                    <!-- <i id="right" class="fa-solid fa-chevron-right"></i> -->
-
-                </div>
-
-
+                    <div class="product-box"> 
+                        @foreach($products as $key => $product)
+                            @if(count($product->images)>0)
+                            <div class="product-item">
+                                <img src="uploads/products/{{ $product->images[0]->name }}" alt=""> 
+                                <div class="card-body">
+                                    <div class="product-info">
+                                        <a href="/product/{{ $product->uuid }}">{{ $product->name }}</a>
+                                    </div>
+                                    <div class="intro">
+                                        <span class="last-price"> 
+                                            {{ $product->price }} AZN
+                                        </span>
+                                        <div class="reyting">
+                                            <i class="fa-solid fa-star"></i>
+                                            <i class="fa-solid fa-star"></i>
+                                            <i class="fa-solid fa-star"></i>
+                                            <i class="fa-solid fa-star"></i>
+                                            <i class="fa-solid fa-star"></i> 
+                                        </div> 
+                                    </div>
+                                </div> 
+                            </div> 
+                            @endif
+                        @endforeach 
+                    </div> 
+ 
             </div>
         </div>
     </section>
@@ -223,18 +202,6 @@
                 Abunəçilərə birinci ay üçün xidmət ödənişsizdir 
             </div> 
         </div> 
-    </section>
-
-
-    <!-- SECTİN COMPANY LOGO -->
-    <section class="section-client-logo">
-        <div class="custom-container"> 
-            <div class="d-flex flex-wrap">
-                @foreach($centers as $key => $company)
-                <div class="client-logo"> <img src="uploads/{{ $company->picture }}" alt=""></div>
-                @endforeach
-            </div> 
-        </div>
     </section>
 
 @endsection
