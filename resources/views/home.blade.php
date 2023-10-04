cd3ad2d3082622276341f86512a19ef38368b8f2
amid haqverdiyev <amid.haqverdiyev@limestonedigital.com>
2023-10-04 14:32:16 +0400

    enhancement of general view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,14 +12,14 @@
             <div class="carousel-inner">
                 <div class="carousel-item active" data-bs-interval="10000">
                     <img src="assets/img/banner-1.png" class="d-block w-100" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
+                    <div class="carousel-caption d-md-block">
                         <h3>First slide label</h3>
                         <p>Some representative placeholder content for the first slide.</p> 
                     </div>
                 </div>
                 <div class="carousel-item" data-bs-interval="2000">
                     <img src="assets/img/banner-2.png" class="d-block w-100" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
+                    <div class="carousel-caption d-md-block">
                         <h3>Second slide label</h3>
                         <p>Some representative placeholder content for the first slide.</p>
                     </div>
